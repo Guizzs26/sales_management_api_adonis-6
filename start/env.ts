09c9f9f012931f8ee28d53318bd0fc97230517db
Guilherme <guiteixeira1926@gmail.com>
2025-01-08 22:42:22 -0400

    feat(config): adicionar configuração inicial do banco de dados com docker e pgadmin utilizando o docker compose e as validações corretas das variáveis de ambiente

--- start/env.ts
+++ start/env.ts
@@ -23,9 +23,11 @@ export default await Env.create(new URL('../', import.meta.url), {
   | Variables for configuring database connection
   |----------------------------------------------------------
   */
-  DB_HOST: Env.schema.string({ format: 'host' }),
-  DB_PORT: Env.schema.number(),
-  DB_USER: Env.schema.string(),
-  DB_PASSWORD: Env.schema.string.optional(),
-  DB_DATABASE: Env.schema.string(),
+  POSTGRES_HOST: Env.schema.string({ format: 'host' }),
+  POSTGRES_PORT: Env.schema.number(),
+  POSTGRES_USER: Env.schema.string(),
+  POSTGRES_PASSWORD: Env.schema.string.optional(),
+  POSTGRES_DB: Env.schema.string(),
+  PGADMIN_EMAIL: Env.schema.string({ format: 'email' }),
+  PGADMIN_PASSWORD: Env.schema.string(),
 })
