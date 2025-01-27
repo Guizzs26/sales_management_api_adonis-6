80e253c3201cf4c469bce6887b0af41fd3017118
Guizzs26 <guiteixeira1926@gmail.com>
2025-01-27 17:10:20 -0300

    feat: adicionar método completo para filtro dashboard relacionado a cliente (tipo e volume de compra)

--- app/controllers/dashboard/dashboard_controller.ts
+++ app/controllers/dashboard/dashboard_controller.ts
@@ -1,16 +1,22 @@
 import type { HttpContext } from '@adonisjs/core/http'
-import Cliente from '#models/cliente/cliente'
 import db from '@adonisjs/lucid/services/db'
 
 export default class DashboardController {
+  // async vendas({ request, response }: HttpContext) {}
+
   async clientes({ request, response }: HttpContext) {
+    const tipo = request.input('tipo', null)
     const minVendas = request.input('min_vendas', 0)
     const maxVendas = request.input('max_vendas', Number.MAX_SAFE_INTEGER)
+    const page = request.input('page', 1)
+    const limit = request.input('limit', 20)
 
     // Contagem de clientes por tipo (PF e PJ)
-    const totalClientesPorTipo = await Cliente.query()
+    const totalClientesPorTipo = await db
+      .from('clientes')
       .select('tipo')
       .count('* as totalClientes')
+      .if(tipo, (query) => query.where('tipo', tipo))
       .groupBy('tipo')
 
     // Contagem total de clientes cadastrados
@@ -20,13 +26,22 @@ export default class DashboardController {
     const totalClientesCadastradosValor = totalClientesCadastrados[0]?.totalClientesCadastrados || 0
 
     // Vendas por cliente (volume de compras)
-    const vendasPorCliente = await Cliente.query()
-      .select('id', 'nomeCompleto', 'tipo')
-      .withCount('vendas', (query) => {
-        query.as('quantidadeVendas')
+    const vendasPorCliente = await db
+      .from((subquery) => {
+        subquery
+          .from('clientes')
+          .select('clientes.id', 'clientes.nome_completo', 'clientes.tipo')
+          .count('vendas.id as vendas_count')
+          .leftJoin('vendas', 'clientes.id', '=', 'vendas.cliente_id')
+          .groupBy('clientes.id', 'clientes.nome_completo', 'clientes.tipo')
+          .as('vendas_dados')
       })
-      .havingBetween('quantidadeVendas', [minVendas, maxVendas])
-      .orderBy('quantidadeVendas', 'desc')
+      .where('vendas_count', '>=', minVendas)
+      .andWhere('vendas_count', '<=', maxVendas)
+      .if(tipo, (query) => query.where('tipo', tipo))
+      .orderBy('vendas_count', 'desc')
+      .limit(limit)
+      .offset((page - 1) * limit)
 
     response.send({
       totalClientesPorTipo,
